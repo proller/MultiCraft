1823a45afc008d29638fa3404134c6d202035e4d
proller <proller@github.com>
2015-11-15 02:12:24 +0300

    start

---
+++ mods/default/multicraft.lua
@@ -0,0 +1,20 @@
+core.register_alias("default:grass", "default:grass_1")
+core.register_alias("flowers:tulip_red", "flowers:tulip")
+core.register_alias("flowers:tulip_pink", "flowers:tulip")
+core.register_alias("flowers:tulip_orange", "flowers:tulip")
+core.register_alias("flowers:tulip_white", "flowers:tulip")
+
+core.register_alias("flowers:paeonia", "flowers:dandelion_yellow")
+core.register_alias("flowers:oxeye_daisy", "flowers:geranium")
+core.register_alias("flowers:houstonia", "flowers:viola")
+
+core.register_alias("flowers:blue_orchid", "flowers:rose")
+core.register_alias("flowers:allium", "flowers:dandelion_white")
+core.register_alias("farming:carrot", "default:dirt")
+
+core.register_alias("farming:potato", "flowers:dandelion_yellow")
+core.register_alias("farming:melon", "flowers:dandelion_yellow")
+core.register_alias("farming:pumpkin_face", "flowers:dandelion_yellow")
+
+
+
